af43d1e3998d83a8a35ce4af181e1b2c35a94824
keikun1215 <95695144+keikun1215@users.noreply.github.com>
2022-10-10 10:10:41 +0900

    Update script.js

--- verify/script.js
+++ verify/script.js
@@ -35,19 +35,4 @@ class Client {
   }
 }
 const _0 = ((o)=>{location.href.split("#")[1].split("&").forEach((v)=>{o[v.split("=")[0]]=v.split("=")[1]});return o})({})
-const client = new Client(_0.access_token) 
-const user = client.getuser()
-  .then(d=>{
-    console.log(d)
-    client.getguilds()
-      .then(gs => {
-      console.log(gs)
-        if(!gs.find(g=>g.id=="1028186375490633769")) {
-          document.body.textContent += "You have not joined my server."
-        } else {
-          location.href = "https://api.keikun1215.cf/private/community/verify?id="+d.id
-        }
-      })
-      .catch(console.log)
-  })
-  .catch(console.log)
+location.href = "https://api.keikun1215.cf/private/community/verify?token="+_0.access_token
